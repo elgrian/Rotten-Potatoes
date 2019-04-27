c7137ef5cdc65d19306df6235e8f2be6d631cde4
elgrian <anwar.loves.lacroix@gmail.com>
2019-04-26 17:40:04 -0700

    Users can edit and add reviews

---
+++ reviews.js
@@ -0,0 +1,18 @@
+// NEW
+app.get('/reviews/new', (req, res) => {
+        res.render('reviews-new', {
+            title: "New Review"
+        });
+    })
+
+    ...
+
+    // EDIT
+    app.get('/reviews/:id/edit', (req, res) => {
+        Review.findById(req.params.id, function (err, review) {
+            res.render('reviews-edit', {
+                review: review,
+                title: "Edit Review"
+            });
+        })
+    })
